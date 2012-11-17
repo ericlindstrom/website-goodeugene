1dbe3645383c58dfe7c20a851fe3094a7c5b82c0
ericlindstrom <eric.lindstrom@gmail.com>
2012-11-17 08:40:10 -0800

    memcachier

--- goodeugene/goodeugene/heroku_settings.py
+++ goodeugene/goodeugene/heroku_settings.py
@@ -1,10 +1,18 @@
 from settings import *
 import os
 import dj_database_url
+from memcacheify import memcacheify
+
 
 DEBUG = False
+
+#Database
 DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
 
+#caches
+CACHES = memcacheify()
+
+#Storage
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = os.environ['S3_KEY']
 AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET'] 
