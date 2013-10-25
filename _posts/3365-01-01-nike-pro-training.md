13017999ce36428937536d0ace844abcf5c1d75d
ericlindstrom <eric.lindstrom@gmail.com>
2013-10-24 18:48:29 -0700

    updates

--- _posts/3365-01-01-nike-pro-training.md
+++ _posts/3365-01-01-nike-pro-training.md
@@ -9,7 +9,7 @@ thumb: /media/images/NPT20_Sparq_coach_manual-thumb.jpg
 ![](/media/images/NPT20_Sparq_Detail_01.jpg)
 ![](/media/images/NPT20_Sparq_Detail_02.jpg)
 
-Nemo Design
-Client: Nike Athletic Training / Sparq
-Art Direction/Designer: Eugene Good
+Nemo Design<br/>
+Client: Nike Athletic Training / Sparq<br/>
+Art Direction/Designer: Eugene Good<br/>
 Design Director: Jeff Weithman
--- media/static/js/goodeugene.js
+++ media/static/js/goodeugene.js
@@ -1,36 +1,37 @@
-/**
- * jQuery Unveil
- * A very lightweight jQuery plugin to lazy load images
- * http://luis-almeida.github.com/unveil
- *
- * Licensed under the MIT license.
- * Copyright 2013 Luís Almeida
- * https://github.com/luis-almeida
- */
+require.config({
+  paths: {
+    jquery: ['//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min', 
+      '/media/static/js/vendor/jquery-1.10.1.min'],
+    unveil: '/media/static/js/vendor/unveil'
+  },
+  shim: {
+    jquery: { exports: '$' },
+    unveil: ['jquery']
+  }
+})
 
-;(function($){$.fn.unveil=function(threshold){var $w=$(window),th=threshold||0,retina=window.devicePixelRatio>1,attrib=retina?"data-src-retina":"data-src",images=this,loaded,inview,source;this.one("unveil",function(){source=this.getAttribute(attrib);source=source||this.getAttribute("data-src");if(source)this.setAttribute("src",source);});function unveil(){inview=images.filter(function(){var $e=$(this),wt=$w.scrollTop(),wb=wt+$w.height(),et=$e.offset().top,eb=et+$e.height();return eb>=wt-th&&et<=wb+th;});loaded=inview.trigger("unveil");images=images.not(loaded);}$w.scroll(unveil);$w.resize(unveil);unveil();return this;};})(window.jQuery||window.Zepto);
-
-
-$(function() {
-  $('#header a[href="/info/"]').on('click', function(e) {
-    e.preventDefault();
-    var info = $('meta[name="info"]').attr('content').replace(/\|/gi, '<br/>');
-    $('.content_wrap').prepend(['<div class="info_block block border_bottom"><div class="position"><div class="container">', info, '<a class="close" href="#"> × </a></div></div></div>'].join(''));
-
-    $('.info_block .close').on('click', function(e) {
+require(['jquery', 'unveil'], function($) {
+  $(function() {
+    $('#header a[href="/info/"]').on('click', function(e) {
       e.preventDefault();
-      $('.info_block').remove();
+      var info = $('meta[name="info"]').attr('content').replace(/\|/gi, '<br/>');
+      $('.content_wrap').prepend(['<div class="info_block block border_bottom"><div class="position"><div class="container">', info, '<a class="close" href="#"> × </a></div></div></div>'].join(''));
+  
+      $('.info_block .close').on('click', function(e) {
+        e.preventDefault();
+        $('.info_block').remove();
+      });
+  
+      if ($('body').hasClass('info_open')) {
+        $('.info_block').remove();
+        $('body').removeClass('info_open');
+      } else {
+        $('body').addClass('info_open');
+        window.scrollTo(0,0);
+      }
     });
-
-    if ($('body').hasClass('info_open')) {
-      $('.info_block').remove();
-      $('body').removeClass('info_open');
-    } else {
-      $('body').addClass('info_open');
-      window.scrollTo(0,0);
-    }
+  
+  
+    $('img').unveil(200);
   });
-
-
-  $('img').unveil(200);
 });
